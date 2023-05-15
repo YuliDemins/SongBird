8ecce1dd5742918aa87b4597dfc65e11adf79136
Yulia <yudemins@mail.ru>
2023-05-15 19:03:11 +0300

    feat: add webpack and plugins

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// import './style.scss'
+// import './assets/svg/rsschool.svg';
+
+
+// import header from './components/header/header.js'
+
+// import createStartPage from './components/pages/main';
+
+// import footer from './components/footer/footer.js'
+
+let root = document.getElementById('root');
+
+// header.setHeader()
+
+// createStartPage()
+
+// footer.setFooter()
